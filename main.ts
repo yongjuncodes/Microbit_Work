d75143cf16f308dcaafca3e0998b76b3a0cb99a4
yongjuncodes <138283014+yongjuncodes@users.noreply.github.com>
2024-06-21 00:50:30 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,23 @@
 radio.onReceivedNumber(function (receivedNumber) {
     v = receivedNumber
 })
+radio.onReceivedString(function (receivedString) {
+    msg = receivedString
+})
+let msg = ""
 let v = 0
+basic.showString("R")
 radio.setGroup(1)
 basic.forever(function () {
-    if (v == 1) {
+    if (msg == "F") {
         TPBot.setTravelSpeed(TPBot.DriveDirection.Forward, 50)
-    } else if (v == 2) {
+    } else if (msg == "B") {
         TPBot.setTravelSpeed(TPBot.DriveDirection.Backward, 50)
-    } else if (v == 3) {
+    } else if (msg == "L") {
         TPBot.setTravelSpeed(TPBot.DriveDirection.Left, 50)
-    } else if (v == 4) {
+    } else if (msg == "R") {
         TPBot.setTravelSpeed(TPBot.DriveDirection.Right, 50)
-    } else if (v == 0) {
+    } else {
         TPBot.stopCar()
     }
 })
